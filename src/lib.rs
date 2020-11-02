5c50d0f5f09fc0840af724036b26353cd524163f
LambdaXymox <lambda.xymox@gmail.com>
2020-11-01 23:09:33 -0600

    Define component array type.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ use bitset::{
 };
 
 use std::collections::{
-    VecDeque
+    VecDeque,
+    HashMap,
 };
 use std::fmt;
 
@@ -120,5 +121,58 @@ impl EntityManager {
 
     pub fn get_signature(&self, entity: Entity) -> Signature {
         self.signatures[entity.id as usize]
+    }
+}
+
+
+struct ComponentArray<T> {
+    data: Vec<T>,
+    entity_to_index_map: HashMap<Entity, usize>,
+    index_to_entity_map: HashMap<usize, Entity>,
+    length: usize,
+    capacity: usize,
+}
+
+impl<T> ComponentArray<T> where T: Copy {
+    pub fn new(capacity: usize) -> ComponentArray<T> {
+        ComponentArray {
+            data: Vec::with_capacity(capacity),
+            entity_to_index_map: HashMap::new(),
+            index_to_entity_map: HashMap::new(),
+            length: 0,
+            capacity: capacity,
+        }
+    }
+
+    pub fn insert(&mut self, entity: Entity, component: T) {
+        let new_index = self.length;
+        self.entity_to_index_map.insert(entity, new_index);
+        self.index_to_entity_map.insert(new_index, entity);
+        self.data[new_index] = component;
+        self.length += 1;
+    }
+
+    pub fn remove(&mut self, entity: Entity) {
+        let index_of_removed_entity = self.entity_to_index_map[&entity];
+        let index_of_last_element = self.length - 1;
+        self.data[index_of_removed_entity] = self.data[index_of_last_element];
+
+        let entity_of_last_element = self.index_to_entity_map[&index_of_last_element];
+        self.entity_to_index_map.insert(entity_of_last_element, index_of_removed_entity);
+        self.index_to_entity_map.insert(index_of_removed_entity, entity_of_last_element);
+
+        self.entity_to_index_map.remove(&entity);
+        self.index_to_entity_map.remove(&index_of_last_element);
+        self.length -= 1;
+    }
+
+    pub fn get(&self, entity: Entity) -> Option<&T> {
+        self.data.get(self.entity_to_index_map[&entity])
+    }
+
+    pub fn destroy_entity(&mut self, entity: Entity) {
+        if self.entity_to_index_map.get(&entity).is_some() {
+            self.remove(entity);
+        }
     }
 }
